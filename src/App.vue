89c1487ae47d4fd50f5618a12929a0c3da466baa
Igor Myasnikov <igorpilium@gmail.com>
2021-02-20 08:17:13 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,625 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="patch">
+    <div class="patch__section">
+      <div class="pacth__legend">
+        <p>Куда уходит</p>
+        <p>Номер порта</p>
+        <p>Что сидит</p>
+      </div>
+      <div class="patch__inner">
+        <div class="port" v-for="(port, index) in ports1" :key="index">
+          <p>{{ port.destination }}</p>
+          <p class="port__number">{{ port.port }}</p>
+          <p>{{ port.place }}</p>
+        </div>
+      </div>
+    </div>
+    <div class="patch__section">
+      <div class="pacth__legend">
+        <p>Куда уходит</p>
+        <p>Номер порта</p>
+        <p>Что сидит</p>
+      </div>
+      <div class="patch__inner">
+        <div class="port" v-for="(port, index) in ports2" :key="index">
+          <p>{{ port.destination }}</p>
+          <p class="port__number">{{ port.port }}</p>
+          <p>{{ port.place }}</p>
+        </div>
+      </div>
+    </div>
+    <div class="patch__section">
+      <div class="pacth__legend">
+        <p>Куда уходит</p>
+        <p>Номер порта</p>
+        <p>Что сидит</p>
+      </div>
+      <div class="patch__inner">
+        <div class="port" v-for="(port, index) in ports3" :key="index">
+          <p>{{ port.destination }}</p>
+          <p class="port__number">{{ port.port }}</p>
+          <p>{{ port.place }}</p>
+        </div>
+      </div>
+    </div>
+    <div class="patch__section">
+      <div class="pacth__legend">
+        <p>Куда уходит</p>
+        <p>Номер порта</p>
+        <p>Что сидит</p>
+      </div>
+      <div class="patch__inner">
+        <div class="port" v-for="(port, index) in ports4" :key="index">
+          <p>{{ port.destination }}</p>
+          <p class="port__number">{{ port.port }}</p>
+          <p>{{ port.place }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      ports1: [
+        {
+          port: 1,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 2,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 3,
+          destination: "-",
+          place: "Лаба",
+        },
+        {
+          port: 4,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 5,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 6,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 7,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 8,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 9,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 10,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 11,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 12,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 13,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 14,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 15,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 16,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 17,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 18,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 19,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 20,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 21,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 22,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 23,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 24,
+          destination: "-",
+          place: "-",
+        },
+      ],
+      ports2: [
+        {
+          port: 1,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 2,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 3,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 4,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 5,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 6,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 7,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 8,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 9,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 10,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 11,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 12,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 13,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 14,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 15,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 16,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 17,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 18,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 19,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 20,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 21,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 22,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 23,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 24,
+          destination: "-",
+          place: "-",
+        },
+      ],
+      ports3: [
+        {
+          port: 1,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 2,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 3,
+          destination: "-",
+          place: "Лаба(резерв)",
+        },
+        {
+          port: 4,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 5,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 6,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 7,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 8,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 9,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 10,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 11,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 12,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 13,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 14,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 15,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 16,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 17,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 18,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 19,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 20,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 21,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 22,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 23,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 24,
+          destination: "-",
+          place: "-",
+        },
+      ],
+      ports4: [
+        {
+          port: 1,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 2,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 3,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 4,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 5,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 6,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 7,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 8,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 9,
+          destination: "-",
+          place: "305(Р)",
+        },
+        {
+          port: 10,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 11,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 12,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 13,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 14,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 15,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 16,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 17,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 18,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 19,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 20,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 21,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 22,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 23,
+          destination: "-",
+          place: "-",
+        },
+        {
+          port: 24,
+          destination: "-",
+          place: "-",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  font-size: 12px;
+  margin: 0;
+  padding: 0;
+}
+
+.patch > * {
+  display: flex;
+  justify-content: space-between;
+  height: 20vh;
+  padding: 16px;
+}
+
+.pacth__legend {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 5%;
+  padding: 16px 0;
+  color: #3c5d70;
+  font-weight: bold;
+}
+
+.pacth__legend p {
+  margin: 0;
+  margin-bottom: 36px;
+}
+
+.pacth__legend p:first-child { margin-top: 8px; }
+
+.patch__inner {
+  display: flex;
+  width: 95%;
+}
+
+.port {
+  width: calc(100% / 24);
+  padding: 16px 0;
+}
+
+.port p {
+  margin: 0;
+  margin-bottom: 16px;
+  padding: 8px;
+  text-align: center;
+  color: #282828;
+  background-color: hsl(202, 64%, 80%);
+}
+
+.port__number {
+  font-size: 20px;
+}
+
+.kommutator {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
+
+@media print {
+  .pacth__legend { display: none; }
+}
+
+@media print {
+  .patch__inner { width: 100%; }
 }
 </style>
